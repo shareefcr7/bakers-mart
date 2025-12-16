f26966cef97e56d5644907dab6e6d986ed6dffa4
Shareef <shareefnncr@gmail.com>
2025-12-16 14:07:48 +0530

    Layout: Use 2-column grid for products on mobile

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -50,7 +50,7 @@ function ProductContent() {
             
             {/* Product Grid */}
             <div className="flex-1">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
                     <AnimatePresence mode="popLayout">
                         {filteredProducts.map(product => (
                             <motion.div
